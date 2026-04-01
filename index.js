4036d812bde7a1955eb8cd56394859ffa7d512ef
ruben-9091 <ruben_9091@hotmail.com>
2026-04-01 21:35:54 +0200

    Solved lab

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 // Iteration 1: Names and Input
+const hacker1 = "Ruben";
+console.log (`The driver's name is ${hacker1}`);
+
+const hacker2 = "Silvia"
+console.log (`The navigator's name is ${hacker2}`);
 
 
 // Iteration 2: Conditionals
-
+if (hacker1.length > hacker2.length) {
+    console.log (`The driver has the longest name, it has ${hacker1.length} characters.`);
+} else if (hacker1.length < hacker2.length) {
+    console.log (`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`);
+} else {
+    console.log (`Wow, you both have equally long names, ${hacker1.length} characters!`);
+}
 
 // Iteration 3: Loops
+const varHacker1 = hacker1.toUpperCase().split("").join(" ");
+const varHacker2 = hacker2.split("").reverse().join("");
+
+if (hacker1 > hacker2) {
+    console.log ("The driver's name goes first.");
+} else if (hacker1 < hacker2) {
+    console.log ("Yo, the navigator goes first, definitely.");
+} else {
+    console.log ("What?! You both have the same name?");
+}
